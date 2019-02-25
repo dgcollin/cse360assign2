875a99b6e99c4c37a031b96d8099dca47bf7bb76
danielcollins <danielcollins@daniels-mbp-2>
2019-02-24 20:14:39 -0700

    commented version of Calculator class

--- cse360assign2/src/cse360assign2/Calculator.java
+++ cse360assign2/src/cse360assign2/Calculator.java
@@ -1,33 +1,62 @@
+/**
+ * @author - Daniel Collins
+ * Class ID: 351
+ * Assignment #2
+ * Description: This class performs various mathematical operations on an integer entered by the user and can also return the 
+ * 				history of said operations.
+ */
 package cse360assign2;
 
 public class Calculator {
 
 	private int total;
-	
+	/**
+	 * Initializes the calculator.
+	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
-	
+	/**
+	 * Returns the current total.
+	 */
 	public int getTotal () {
 		return 0;
 	}
-	
+	/**
+	 * Adds a user entered value to the total.
+	 * 
+	 * @param value the number that will be added to the total.
+	 */
 	public void add (int value) {
 		
 	}
-	
+	/**
+	 * Subtracts a user entered value from the total.
+	 * 
+	 * @param value the number that will be subtracted from the total.
+	 */
 	public void subtract (int value) {
 		
 	}
-	
+	/**
+	 * Multiplies the total by a user entered value.
+	 * 
+	 * @param value the number that will be multiplied by the total.
+	 */
 	public void multiply (int value) {
 		
 	}
-	
+	/**
+	 * Divides the total by a user entered value.
+	 * 
+	 * @param value the number that the total will be divided by.
+	 */
 	public void divide (int value) {
 		
 	}
-	
+	/**
+	 * Returns the history of all actions performed by the Calculator.
+	 */
 	public String getHistory () {
 		return "";
 	}
